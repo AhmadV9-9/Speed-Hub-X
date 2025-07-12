cf2a6e414297bff2dd059d1b619773b3760404dc
AhmadV9-9 <sycharles1031@yahoo.com>
2025-07-12 21:27:45 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/0qtCM1WZcy/0", true))()
+
